604002d228120bda1cd4beae4bb0db25b34550e1
JMazlik01 <jakub.mazel@icloud.com>
2023-10-18 17:26:13 +0200

    Updated README, TM as objects

--- README.md
+++ README.md
@@ -1,2 +1,107 @@
 # turing-machine
+
 Simple Turing machine in CL.
+
+
+## How to use
+
+Create new turing machine by calling `turing-machine` function
+
+```Lisp
+; args are instructions, initial-state, final-state (list) and optional description
+> (turing-machine nil 'q1 '(q2 q3) "Example turing machine")
+
+(:INSTRUCTIONS NIL 
+ :INITIAL-STATE Q1 
+ :FINAL-STATE (Q2 Q3) 
+ :DESCRIPTION "Example turing machine")
+
+> (instructions *)
+
+"Example turing machine"
+```
+
+You can add new instructions with `add-instruction` function and `instruction` constructor
+
+```Lisp
+; Adds instruction (Q1, b) -> (Q2, 0, -1) into turing-machine
+> (add-instruction (instruction 'q1 1 'q2 0 -1) tm)
+```
+With complete instruction set you can now start Turing machine on input using `tm-compute`:
+```Lisp
+
+> (tm-compute "a" tm)
+
+"aa"
+
+; if optional last arg is t, function prints configuration in every step
+> (tm-compute "a" tm t)
+
+0:   Q1a ->
+1:   xQ2 ->
+2:   Q3xx ->
+3:   Q3bxx ->
+4:   bQ5xx ->
+5:   baQ5x ->
+6:   baaQ5 ->
+7:   baaQFb ->
+"aa"
+```
+
+You can print all machines instructions with `print-tm-instructions`:
+```Lisp
+> (print-tm-instructions tm2)
+
+(Q2, b) -> (QF, b, 0)
+(Q2, 0) -> (Q2, 1, 1)
+(Q2, 1) -> (Q2, 0, 1)
+T
+```
+
+## Examples
+
+```Lisp
+; this turing machine converts "aaa" -> "aaaa" (or w -> ww where w = a*)
+; b is blank
+
+(prog1 
+  (setf tm (turing-machine nil 'q1 '(qf) "For word w returns ww"))
+  (add-instruction (instruction 'q1 'a 'q2 'x 1) tm)
+  (add-instruction (instruction 'q1 'b 'qf 'b 0) tm)
+
+  (add-instruction (instruction 'q2 'a 'q2 'a 1) tm)
+  (add-instruction (instruction 'q2 'x 'q2 'x 1) tm)
+  (add-instruction (instruction 'q2 'b 'q3 'x -1) tm)
+
+  (add-instruction (instruction 'q3 'a 'q4 'a -1) tm)
+  (add-instruction (instruction 'q3 'x 'q3 'x -1) tm)
+  (add-instruction (instruction 'q3 'b 'q5 'b 1) tm)
+
+  (add-instruction (instruction 'q4 'a 'q4 'a -1) tm)
+  (add-instruction (instruction 'q4 'x 'q1 'x 1) tm)
+
+  (add-instruction (instruction 'q5 'x 'q5 'a 1) tm)
+  (add-instruction (instruction 'q5 'b 'qf 'b 0) tm))
+
+(tm-compute "aaa" tm t)
+```
+
+```Lisp
+; this turing machine flips all bits
+
+(prog1 
+  (setf tm2 (turing-machine nil 'q2 '(qf) "Flips all bits"))
+  (add-instruction (instruction 'q2 '1 'q2 '0 1) tm2)
+  (add-instruction (instruction 'q2 '0 'q2 '1 1) tm2)
+  (add-instruction (instruction 'q2 'b 'qf 'b 0) tm2))
+
+(tm-compute "1010111" tm2 t)
+
+; You can also chain calls
+(tm-compute (tm-compute "1010111" tm2) tm2) ; returns input
+```
+
+## Notes
+
+- Symbol b is reserved for blank character
+- Every symbol from alphabet is just one character long (can't have a1 character)
--- main.lisp
+++ main.lisp
@@ -1,15 +1,12 @@
 
-; TM CONSTRUCTORS
+(defvar *current-tm* nil)
 
-(defvar *instructions* nil)
+;;; TM INSTRUCTIONS
 
 (defun instruction (q-ini g-ini q-fin g-fin mv)
   (list (list q-ini g-ini) '-> (list q-fin g-fin mv)))
 
-(defun add-instruction (instruction)
-  (push instruction *instructions*))
-
-;;; INITIAL STATE ACCESSORS
+;; initial state accessors
 
 (defun ini (instruction)
   (first instruction))
@@ -20,7 +17,7 @@
 (defun instruction-ini-char (instruction)
   (second (ini instruction)))
 
-;;; FINAL STATE ACCESSORS
+;; final state accessors
 
 (defun fin (instruction)
   (third instruction))
@@ -34,13 +31,15 @@
 (defun instruction-fin-move (instruction)
   (third (fin instruction)))
 
-;;; INITIAL STATE
+(defun print-instruction (instruction)
+  (format t "(~a, ~(~a~)) -> (~a, ~(~a~), ~a)~%"
+          (instruction-ini-state instruction)
+          (instruction-ini-char instruction)
+          (instruction-fin-state instruction)
+          (instruction-fin-char instruction)
+          (instruction-fin-move instruction)))
 
-(defun init-state ()
-  (instruction-ini-state (car (last *instructions*))))
-
-
-;;; CONFIGURATION ACCESSORS
+;;; CONFIGURATION FUNCTIONS
 
 (defun string-move-right (string)
   "Moves last character of string to beginning"
@@ -54,13 +53,13 @@
     (format nil "~a~a" (subseq string 1) first-char)))
 
 (defun conf-state-pos (conf)
+  "Returns index position of state in configuration"
   (position #\Q conf))
 
 (defun conf-state (conf)
+  "Returns symbol of current state"
   (let ((pos (conf-state-pos conf)))
-    (read-from-string (subseq conf
-                              pos
-                              (+ pos 2)))))
+    (read-from-string (subseq conf pos (+ pos 2)))))
 
 (defun state-on-beginning-p (conf)
   (= (conf-state-pos conf) 0))
@@ -70,13 +69,50 @@
      (- (length conf) 2)))
 
 (defun conf-next-char (conf)
+  "Returns current character read by head"
   (if (state-on-end-p conf)
       'b
     (read-from-string (string (elt conf (+ (conf-state-pos conf) 2))))))
 
+
+;;; TURING MACHINE
+
+(defun turing-machine (instructions initial-state final-state &optional (description nil))
+  (list :instructions instructions
+        :initial-state initial-state
+        :final-state final-state
+        :description description))
+
+(defun instructions (tm)
+  (getf tm :instructions))
+
+(defun (setf instructions) (value tm)
+  (setf (getf tm :instructions) value))
+
+(defun add-instruction (instruction turing-machine)
+  (push instruction (instructions turing-machine)))
+
+(defun initial-state (tm)
+  (getf tm :initial-state))
+
+(defun final-state (tm)
+  (getf tm :final-state))
+
+(defun description (tm)
+  (getf tm :description))
+
+(defun tm-end-p (tm conf)
+  (find (conf-state conf) (final-state tm)))
+
+(defun print-tm-instructions (tm)
+  (mapcar #'print-instruction (instructions tm))
+  t)
+
+
 ;;; MOVE STATE FUNCTIONS
 
 (defun move-state-right (conf)
+  "Moves state right in config"
   (if (state-on-end-p conf)
       (format nil "~ab~a" (subseq conf 0 (- (length conf) 2)) (conf-state conf))
     (let* ((state-pos (conf-state-pos conf))
@@ -88,6 +124,7 @@
         conf))))
 
 (defun move-state-left (conf)
+  "Moves state left in config"
   (if (state-on-beginning-p conf)
       (format nil "~ab~a" (conf-state conf) (subseq conf 2))
     (let* ((state-pos (conf-state-pos conf))
@@ -99,17 +136,21 @@
         conf))))
 
 (defun conf-move-state (conf n)
+  "Moves state by n"
   (cond ((= n 0) conf)
         ((= n 1) (move-state-right conf))
         ((= n -1) (move-state-left conf))
         (t (error "Connot move state by ~a." n))))
 
+
 ;;; REWRITE CHAR FUNCTIONS
 
 (defun symbol-to-lower (s)
+  "Returns symbol s as lowercase character"
   (char (format nil "~(~a~)" s) 0))
 
 (defun conf-rewrite-char (conf symbol-char)
+  "Rewrites next character"
   (if (state-on-end-p conf)
       (format nil "~a~(~a~)" conf symbol-char)
     (progn
@@ -118,9 +159,11 @@
        (symbol-to-lower symbol-char))
       conf)))
 
+
 ;;; CHANGE STATE FUNCTIONS
 
 (defun conf-change-state (conf new-state)
+  "Changes state in configuration"
   (setf (subseq conf 
                 (conf-state-pos conf)
                 (+ (conf-state-pos conf) 2))
@@ -131,17 +174,21 @@
 ;;; OUTPUT CLEANING FUNCTIONS
 
 (defun front-clean-blank (string)
-  (if (char= (elt string 0) #\b)
+  "Removes blank characters from start of configuration"
+  (if (and (not (string-equal string "")) 
+           (char= (elt string 0) #\b))
       (front-clean-blank (subseq string 1))
     string))
 
 (defun clean-blank (string)
+  "Removes blank characters fron configuration"
   (reverse
    (front-clean-blank 
     (reverse
      (front-clean-blank string)))))
 
 (defun clean-output (conf)
+  "Removes state and blank symbols"
   (labels ((list-to-string (lst)
              (format nil "~{~a~}" lst)))
     (loop
@@ -150,54 +197,74 @@
                   (char= char #\F)) collect char into result
        finally (return (clean-blank (list-to-string result))))))
 
+
 ;;; TM FUNCTIONS
 
-(defun tm-next-instruction (conf)
+(defun tm-next-instruction (tm conf)
+  "Returns next instruction to apply"
   (or
    (find (list (conf-state conf)
                (conf-next-char conf))
-         *instructions* 
+         (instructions tm)
          :key #'ini
          :test #'equal)
    (error "Could not find instruction, check input")))
 
-(defun tm-step (conf)
-  (let ((ins (tm-next-instruction conf)))
+(defun tm-step (tm conf)
+  (let ((ins (tm-next-instruction tm conf)))
     (conf-change-state
      (conf-move-state
       (conf-rewrite-char conf (instruction-fin-char ins))
       (instruction-fin-move ins))
      (instruction-fin-state ins))))
 
-(defun tm-compute (input &optional (print nil))
-  (let* ((state (init-state))
+(defun tm-compute (input tm &optional (print nil))
+  "Computes output for given input"
+  (let* ((state (initial-state tm))
          (ini-input (format nil "~a~a" state input)))
     (loop
-       for input = ini-input then (tm-step input)
+       for input = ini-input then (tm-step tm input)
        for i = 0 then (1+ i)
        when print do (format t "~a:~5t~a ->~%" i input)
-       when (eql (conf-state input) 'QF) do (return (clean-output input))
+       when (tm-end-p tm input) do (return (clean-output input))
        when (< 10000 i) do (error "Infinite computation"))))
 
 #|
-; this turing machine converts "aaa" -> "aaaa" (or w -> ww where w = a*)
-; b is blank
+EXAMPLES
 
-(add-instruction (instruction 'q1 'a 'q2 'x 1))
-(add-instruction (instruction 'q1 'b 'qf 'b 0))
+(prog1 
+  (setf tm (turing-machine nil 'q1 '(qf) "For word w returns ww"))
+  (add-instruction (instruction 'q1 'a 'q2 'x 1) tm)
+  (add-instruction (instruction 'q1 'b 'qf 'b 0) tm)
 
-(add-instruction (instruction 'q2 'a 'q2 'a 1))
-(add-instruction (instruction 'q2 'x 'q2 'x 1))
-(add-instruction (instruction 'q2 'b 'q3 'x -1))
+  (add-instruction (instruction 'q2 'a 'q2 'a 1) tm)
+  (add-instruction (instruction 'q2 'x 'q2 'x 1) tm)
+  (add-instruction (instruction 'q2 'b 'q3 'x -1) tm)
 
-(add-instruction (instruction 'q3 'a 'q4 'a -1))
-(add-instruction (instruction 'q3 'x 'q3 'x -1))
-(add-instruction (instruction 'q3 'b 'q5 'b 1))
+  (add-instruction (instruction 'q3 'a 'q4 'a -1) tm)
+  (add-instruction (instruction 'q3 'x 'q3 'x -1) tm)
+  (add-instruction (instruction 'q3 'b 'q5 'b 1) tm)
 
-(add-instruction (instruction 'q4 'a 'q4 'a -1))
-(add-instruction (instruction 'q4 'x 'q1 'x 1))
+  (add-instruction (instruction 'q4 'a 'q4 'a -1) tm)
+  (add-instruction (instruction 'q4 'x 'q1 'x 1) tm)
 
-(add-instruction (instruction 'q5 'x 'q5 'a 1))
-(add-instruction (instruction 'q5 'b 'qf 'b 0))
+  (add-instruction (instruction 'q5 'x 'q5 'a 1) tm)
+  (add-instruction (instruction 'q5 'b 'qf 'b 0) tm))
+
+(tm-compute "aaa" tm t)
+
+
+
+(prog1 
+  (setf tm2 (turing-machine nil 'q2 '(qf) "Flips all bits"))
+  (add-instruction (instruction 'q2 '1 'q2 '0 1) tm2)
+  (add-instruction (instruction 'q2 '0 'q2 '1 1) tm2)
+  (add-instruction (instruction 'q2 'b 'qf 'b 0) tm2))
+
+(tm-compute "1010111" tm2 t)
+
+You can also chain calls:
+
+(tm-compute (tm-compute "1010111" tm2) tm2)
 
 |#
